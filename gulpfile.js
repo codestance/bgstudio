085fef2d2775b3f54c78e9c14347c0d6de8e6a9c
Unknown <skrzynska.konstancja@gmail.com>
2018-10-06 23:36:56 +0200

    some custom styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('styles', function () {
+    'use strict';
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('stylesheets'));
+});
+
+gulp.task('sync', function () {
+    'use strict';
+    browserSync.init(["stylesheets/*.css"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['styles', 'sync'], function () {
+    'use strict';
+    gulp.watch('sass/**/*.scss', ['styles']);
 });
